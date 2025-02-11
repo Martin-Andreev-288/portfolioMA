93680aea34b5bfdcb9defde5ef6a321682e6bf42
Martin Andreev <martin_i_a@abv.bg>
2025-02-11 15:17:56 +0200

    Install the shadcn library
    Install the Button shadcn component.
    Install lucide react.

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 /// <reference types="vitest" />
 /// <reference types="vite/client" />
 
+import path from "path"
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
@@ -12,5 +13,10 @@ export default defineConfig({
     environment: "jsdom",
     setupFiles: ["./src/setupTests.ts"],
     include: ["src/**/*.{test,spec}.{ts,tsx}"]
-  }
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
