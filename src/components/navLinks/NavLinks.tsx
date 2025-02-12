e3122ff0aadaff3b73129dda7331fda958864621
Martin Andreev <martin_i_a@abv.bg>
2025-02-12 23:06:32 +0200

    Change the navbar navlinks styling in order to maintain consistency with the existing theme configuration adapting to dark/light modes

--- src/components/navLinks/NavLinks.tsx
+++ src/components/navLinks/NavLinks.tsx
@@ -10,13 +10,13 @@ function NavLinks() {
             to={link.href}
             key={link.label}
             className={({ isActive }) => {
-              return `capitalize text-gray-600 hover:text-blue-600 transition-colors font-medium tracking-wide relative group ${
+              return `capitalize text-muted-foreground hover:text-primary transition-colors font-medium tracking-wide relative group ${
                 isActive ? "text-primary" : ""
               }`;
             }}
           >
             {link.label}
-            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-blue-600 transition-all group-hover:w-full"></span>
+            <span className="absolute -bottom-1 left-0 w-0 h-0.5 bg-primary transition-all group-hover:w-full"></span>
           </NavLink>
         );
       })}
