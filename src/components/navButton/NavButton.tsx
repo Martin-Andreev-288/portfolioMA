deb85748edd26ff532d076691da42e855d58339d
Martin Andreev <martin_i_a@abv.bg>
2025-02-14 18:41:15 +0200

    Change the carousel navigation buttons positionning

--- src/components/navButton/NavButton.tsx
+++ src/components/navButton/NavButton.tsx
@@ -9,7 +9,7 @@ function NavButton({ direction, onClick }: NavButtonProps) {
   return (
     <button
       type="button"
-      className={`absolute top-[200px] bg-secondary text-secondary-foreground w-8 h-8 grid place-items-center border border-border rounded-md cursor-pointer transition-colors hover:bg-secondary/80 md:w-10 md:h-10 md:text-xl ${
+      className={`absolute top-[285px] bg-secondary text-secondary-foreground w-8 h-8 grid place-items-center border border-border rounded-md cursor-pointer transition-colors hover:bg-secondary/80 md:w-10 md:h-10 md:text-xl ${
         direction === "prev" ? "left-0" : "right-0"
       }`}
       onClick={onClick}
