424421cf8f2752b98cbf9b1282ce43bd839a806e
Martin Andreev <martin_i_a@abv.bg>
2025-02-15 19:35:34 +0200

    Edit the title styling in the about page by extracting their types in the main index file in order to avoid repetition of big quantity of code

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -4,20 +4,16 @@ import { techSkills, softSkills } from "@/utils";
 function About() {
   return (
     <section className="min-h-screen py-9 px-4 sm:px-6 lg:px-8">
-      <h1 className="text-center text-5xl font-bold text-[#333] dark:text-white uppercase tracking-[0.5rem] mb-8 border-b-[0.3rem] border-b-[#333] dark:border-blue-100 border-solid drop-shadow-[0.2rem_0.2rem_1rem_#555]">
+      <h1 className="title text-center text-5xl font-bold text-[#333] border-b-[0.3rem] uppercase tracking-[0.5rem] drop-shadow-[0.2rem_0.2rem_1rem_#555]">
         About me
       </h1>
-      <div className="max-w-7xl mx-auto">
-        <h2 className="text-3xl font-bold text-center mb-8 dark:text-white">
-          Technical Skills
-        </h2>
+      <div className="max-w-7xl mx-auto text-center">
+        <h2 className="title subtitle">Technical Skills</h2>
         <TechSkills skills={techSkills} />
-        <h2 className="text-3xl font-bold text-center my-8 dark:text-white">
-          Soft Skills
-        </h2>
+        <h2 className="title subtitle">Soft Skills</h2>
         <SoftSkills skills={softSkills} />
 
-        <h3>Hobbies</h3>
+        <h2 className="title subtitle">Hobbies</h2>
       </div>
     </section>
   );
