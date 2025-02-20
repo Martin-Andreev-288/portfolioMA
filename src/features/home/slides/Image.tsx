4fa8edde1e579b03d6285f878ea5e37f68bc483d
Martin Andreev <martin_i_a@abv.bg>
2025-02-20 17:20:35 +0200

    Edit the image slide and the image type object in the according data file

--- src/features/home/slides/Image.tsx
+++ src/features/home/slides/Image.tsx
@@ -14,10 +14,10 @@ function Image({ item, index }: ImageProps) {
         alt={`Slide ${index}`}
         className="mt-8 w-48 h-48 md:w-[300px] md:h-[300px] rounded-full object-cover mb-4 border-4 border-primary/20 shadow-lg mx-auto"
       />
-      <h5 className="text-lg uppercase text-primary mb-3">{item.name}</h5>
-      <p className="max-w-[35em] mx-auto mt-8 text-muted-foreground leading-loose">
+      <p className="max-w-[35em] mx-auto mb-3 text-muted-foreground leading-loose">
         {item.quote}
       </p>
+      <h5 className="text-lg uppercase text-primary mt-8">{item.name}</h5>
       <Quote className="text-3xl text-primary mt-4 mx-auto" />
     </>
   );
--- src/utils/home/carouselItems.ts
+++ src/utils/home/carouselItems.ts
@@ -8,19 +8,13 @@ export const carouselItems: Item[] = [
         name: "Martin Andreev",
     },
     {
-        type: "image",
-        src: "https://images.unsplash.com/photo-1535268647677-300dbf3d78d1?q=80&w=3057&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        name: "Whiskers the Kitty",
-        quote: "In a world full of people, be a cat.",
-    },
-    {
         type: "video",
         src: "https://videos.pexels.com/video-files/4549682/4549682-hd_1920_1080_30fps.mp4",
     },
     {
         type: "image",
-        src: "https://images.unsplash.com/photo-1712311953600-81354526ec28?q=80&w=3164&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-        name: "Birdy",
-        quote: "Let your dreams take flight.",
+        src: "https://cacm.acm.org/wp-content/uploads/2010/08/072010_CACMpg41_An-Interview.jpg",
+        name: "Edsger W. Dijkstra",
+        quote: "Much of the excitement we get out of our work is that we don't really know what we are doing.",
     },
 ];
