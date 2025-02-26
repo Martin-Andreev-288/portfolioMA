63b1eb5cbf35242741c604a809f79f5b4dc342f9
Martin Andreev <martin_i_a@abv.bg>
2025-02-26 16:05:36 +0200

    Edit the vrscans library project font picture and technologies description

--- src/utils/portfolio/projects.ts
+++ src/utils/portfolio/projects.ts
@@ -1,10 +1,10 @@
-import projectBg from '@/assets/vrscansAppBg.png';
+import vrscansLibraryAppBg from '@/assets/vrscansAppBg.png';
 
 export const projects = [
     {
         name: "VRScans library",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -12,8 +12,8 @@ export const projects = [
     },
     {
         name: "VRScans library1",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -21,8 +21,8 @@ export const projects = [
     },
     {
         name: "VRScans library2",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -30,8 +30,8 @@ export const projects = [
     },
     {
         name: "VRScans library3",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -39,8 +39,8 @@ export const projects = [
     },
     {
         name: "VRScans library4",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -48,8 +48,8 @@ export const projects = [
     },
     {
         name: "VRScans library5",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -57,8 +57,8 @@ export const projects = [
     },
     {
         name: "VRScans library6",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
@@ -66,8 +66,8 @@ export const projects = [
     },
     {
         name: "VRScans library7",
-        technologies: "React / TypeScript / Tailwind CSS / React Icons / Vitest / Redux",
-        img: projectBg,
+        technologies: "React / TypeScript / Tailwind CSS / Redux / Vitest / Playwright",
+        img: vrscansLibraryAppBg,
         demo: "https://youtu.be/dAvZAEYDoZI",
         link: "https://vrscanslibrarynm.vercel.app/",
         repo: "https://gitlab.com/Nia-Kostadinova/vrscans-library",
